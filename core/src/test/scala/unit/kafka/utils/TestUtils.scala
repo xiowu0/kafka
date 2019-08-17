a89170afb3ee92ae86c5d143e1a502782d9b188c
Lee Dongjin <dongjin@apache.org>
2019-08-18 01:27:35 +0900

    [LI-CHERRY-PICK] [71e20193] KAFKA-8800: Increase poll timeout in poll[Records]UntilTrue (#7211) TICKET = KAFKA-8800 LI_DESCRIPTION = EXIT_CRITERIA = HASH [71e20193e1fe70e10e4c6961a6db14768f6e0c88] ORIGINAL_DESCRIPTION =
    If retrieving metadata during `poll(Duration)` repeatedly takes longer than the
    poll timeout, we don't make progress and `waitUntilTrue` eventually times out
    causing the test to fail. This behaviour differs from the older `poll(long)` that
    would block until metadata retrieval had completed. The flakiness was likely
    introduced when we switched from the latter to the former.
    
    Reviewers: Ismael Juma <ismael@juma.me.uk>
    (cherry picked from commit 71e20193e1fe70e10e4c6961a6db14768f6e0c88)

--- core/src/test/scala/unit/kafka/utils/TestUtils.scala
+++ core/src/test/scala/unit/kafka/utils/TestUtils.scala
@@ -811,7 +811,7 @@ object TestUtils extends Logging {
                     msg: => String,
                     waitTimeMs: Long = JTestUtils.DEFAULT_MAX_WAIT_MS): Unit = {
     waitUntilTrue(() => {
-      consumer.poll(Duration.ofMillis(50))
+      consumer.poll(Duration.ofMillis(100))
       action()
     }, msg = msg, pause = 0L, waitTimeMs = waitTimeMs)
   }
@@ -821,7 +821,7 @@ object TestUtils extends Logging {
                                  msg: => String,
                                  waitTimeMs: Long = JTestUtils.DEFAULT_MAX_WAIT_MS): Unit = {
     waitUntilTrue(() => {
-      val records = consumer.poll(Duration.ofMillis(50))
+      val records = consumer.poll(Duration.ofMillis(100))
       action(records)
     }, msg = msg, pause = 0L, waitTimeMs = waitTimeMs)
   }
